4b80c6feac3d43f8111620f49fac710bbbaad9c5
2018147527 <sjb7773@naver.com>
2021-05-15 13:51:15 +0900

    test22

--- LAB4/base.js
+++ LAB4/base.js
@@ -33,10 +33,11 @@ searchBtn.onclick = selectCategory;
 function infiniteScroll(){
     if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight && !isExecuted){
         isExecuted = true;
-        if(finalGroup.length>6){
+        if(finalGroup.length>6 || firstflag){
             for(let i = 0; i < first_product_list.length; i++) {
                 Storing(first_product_list[i]);
             }
+            firstflag = false;
         }
         setTimeout(() => {
             isExecuted = false;
